9b28b615fd579cd1931e2f1b74551ea2eed62e27
coltscott <83851807+coltscott@users.noreply.github.com>
2022-04-08 00:05:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=west%20lafayette&un
   console.log(data);
 
   var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+  var background = "https://www.publicdomainpictures.net/pictures/40000/velka/blue-clouds-background.jpg";
   var temp = Math.round(data.main.temp);
   var weather = data.weather[0].main;
   var name = data.name;
-   
+  var description = "Currently the weather outside consists of: "
+   $(".background").attr("src", background);
   $('.icon').attr('src', icon);
+  $(".description").append(description);
   $(".name").append(name);
   $(".weather").append(weather);
   $(".temp").append(temp + "°");
