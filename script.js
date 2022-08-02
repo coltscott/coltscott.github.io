35085aab58439278b22c9cf308160172c38eae4c
coltscott <83851807+coltscott@users.noreply.github.com>
2022-08-01 22:18:34 -0400

    added wind speed with graphic

--- script.js
+++ script.js
@@ -4,20 +4,29 @@ var wName = document.querySelector('.name');
 var desc = document.querySelector('.description');
 var temp = document.querySelector('.temp');
 var iconic = document.querySelector('.icon');
+var wind = document.querySelector('.wind-speed');
+$('.icon').attr('src', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQARKISBFy9j6rW3qTcdGSdzwUkCd3AEeMPMQ&usqp=CAU");
+$('.wind-speed-icon').attr('src', "wind-clipart-wind-flow.png");
+$('.wind-speed-icon').css('opacity', 0);
+
 
 function buttonEvent() {
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=c983b5a10bc8da951ebfd1ad2c147a89')
   .then(response =>response.json())
   .then(data => {
+    console.log(data);
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var tempValue = Math.round(data.main.temp) + "°";
     var weather = data.weather[0].main;
     var nameValue = data.name;
     var description = "Currently the weather in " + nameValue + " consists of: " + weather;
+    var windSpeed = "Wind Speed: " + data.wind.speed + " MPH";
 
     $('.icon').attr('src', icon);
     temp.innerHTML = tempValue;
     desc.innerHTML = description;
+    wind.innerHTML = windSpeed;
+    $('.wind-speed-icon').css('opacity', 1);
   })
   
 .catch(err => alert("Unrecognized City Name!"))
